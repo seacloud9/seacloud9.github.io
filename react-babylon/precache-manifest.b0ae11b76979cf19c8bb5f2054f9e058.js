bf71f7594857cb87887c43f63f15c8a752cbd68f
Brendon Smith <brendonsmith@seacloud9.org>
2019-06-23 14:21:01 -0700

    adding react-babylon

---
+++ react-babylon/precache-manifest.b0ae11b76979cf19c8bb5f2054f9e058.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211e11306a7ce4785544cbe3da9eeaa3",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "95723126ebeca38616a3",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d955c90812b5b225c151.bundle.js"
+  },
+  {
+    "url": "runtime~main.d955c90812b5b225c151.bundle.js"
+  },
+  {
+    "url": "vendors~main.d955c90812b5b225c151.bundle.js"
+  }
+]);
